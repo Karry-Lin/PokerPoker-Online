f865ad9fa87c2703f662b2ffb0f11e95e9e4591b
howard522 <8754howard@gmail.com>
2024-10-17 23:00:22 +0800

    feat. lobby css
    feat. lobby css

--- src/app/lobby/page.js
+++ src/app/lobby/page.js
@@ -1,13 +1,24 @@
 'use client';
-import { useState } from 'react';
-import { Form, Card, Container, Row, Col, Button, Modal, Alert } from 'react-bootstrap';
-import { FaLock } from 'react-icons/fa'; // Import lock icon from react-icons
-import styles from './Page.module.css'; // Custom CSS for background image
+import { useEffect, useState } from 'react';
+import {
+  Form,
+  Card,
+  Container,
+  Row,
+  Col,
+  Button,
+  Modal,
+  Alert
+} from 'react-bootstrap';
+import { HiLockClosed, HiLockOpen } from 'react-icons/hi2';
+import NavBar from '../components/navBar';
+import styles from './Page.module.css';
+import { useUserStore } from '@/app/stores/userStore.js';
 
 export default function Page() {
   const rooms = [
     {
-      name: 'room1',
+      name: '叫我大帥哥',
       state: 'Ready',
       id: 1,
       gametype: '13poker',
@@ -16,7 +27,7 @@ export default function Page() {
       password: null
     },
     {
-      name: 'room2',
+      name: '小明生日房',
       state: 'unReady',
       id: 2,
       gametype: 'big2',
@@ -131,6 +142,28 @@ export default function Page() {
   const [passwordInput, setPasswordInput] = useState('');
   const [selectedRoom, setSelectedRoom] = useState(null);
   const [errorMessage, setErrorMessage] = useState('');
+  const [avatar, setAvatar] = useState('');
+
+  const userStore = useUserStore(); // Moved here to be accessible
+
+  // Fetch user data when component mounts
+  useEffect(() => {
+    fetchUserData();
+  }, [userStore.userId]); // Dependency array ensures it runs only when userId changes
+
+  const fetchUserData = async () => {
+    if (userStore.userId) {
+      const response = await fetch(`/api/user?id=${userStore.userId}`, {
+        method: 'GET'
+      });
+      const data = await response.json();
+      if (!response.ok) {
+        console.log(data.error);
+      } else {
+        setAvatar(data.avatar || '');
+      }
+    }
+  };
 
   // Filter rooms based on selected game type and search term
   const filteredRooms = rooms
@@ -172,101 +205,107 @@ export default function Page() {
   };
 
   return (
-    <Container fluid className={styles.page}>
-      <Row>
-        <Col xs={12} md={3} className='p-4'>
-          <Form.Group>
-            <Form.Label>Select Game Type</Form.Label>
+    <>
+      <NavBar avatar={avatar} setAvatar={setAvatar}></NavBar>
+      <Container fluid className={styles.page}>
+        <Row>
+          <Col xs={12} md={1}></Col>
+          <Col xs={12} md={3} className='p-4'>
+            {/* Search bar to filter rooms */}
             <Form.Control
-              as='select'
-              value={selectedGameType}
-              onChange={(e) => setSelectedGameType(e.target.value)}
+              type='text'
+              placeholder='輸入房間名稱'
+              value={searchTerm}
+              onChange={handleSearch}
               className='mb-3'
-            >
-              {gameTypes.map((type) => (
-                <option key={type} value={type}>
-                  {type === 'all' ? 'Select All' : type}
-                </option>
-              ))}
-            </Form.Control>
-          </Form.Group>
-
-          {/* Search bar to filter rooms */}
-          <Form.Control
-            type='text'
-            placeholder='Search room...'
-            value={searchTerm}
-            onChange={handleSearch}
-            className='mb-3'
-          />
-        </Col>
-
-        <Col xs={12} md={7} className='p-4'>
-          {filteredRooms.map((room) => (
-            <Card key={room.id} className='mb-4'>
-              <Card.Body>
-                <Card.Text>
-                  {room.name}
-                  {room.password && (
-                    <FaLock
-                      className='ml-2'
-                      style={{ cursor: 'pointer' }}
-                      title="Password protected" // Show lock icon if room is password protected
-                    />
-                  )}
-                  <br />
-                  <strong>Game Type:</strong> {room.gametype}
-                  <br />
-                  <strong>Current Players:</strong> {room.current_player}/
-                  {room.max_player}
-                  <br />
-                  <strong>Status:</strong> {room.state}
-                  <br />
-                </Card.Text>
-                <Button
-                  variant={
-                    room.current_player === room.max_player
-                      ? 'secondary'
-                      : 'primary'
-                  }
-                  disabled={room.current_player === room.max_player}
-                  onClick={() => handleEnterRoom(room)}
-                >
-                  {room.current_player === room.max_player
-                    ? 'Room Full'
-                    : 'Enter'}
-                </Button>
-              </Card.Body>
-            </Card>
-          ))}
-        </Col>
-      </Row>
-
-      {/* Password modal */}
-      <Modal show={showModal} onHide={() => setShowModal(false)} centered>
-        <Modal.Header closeButton>
-          <Modal.Title>Enter Room Password</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form.Group>
-            <Form.Label>Password:</Form.Label>
-            <Form.Control
-              type="password"
-              value={passwordInput}
-              onChange={(e) => setPasswordInput(e.target.value)}
             />
-          </Form.Group>
-          {errorMessage && <Alert variant="danger" className="mt-3">{errorMessage}</Alert>}
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={() => setShowModal(false)}>
-            Cancel
-          </Button>
-          <Button variant="primary" onClick={handlePasswordSubmit}>
-            Enter Room
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </Container>
+            <Form.Group>
+              <Form.Label>Select Game Type</Form.Label>
+              <Form.Control
+                as='select'
+                value={selectedGameType}
+                onChange={(e) => setSelectedGameType(e.target.value)}
+                className='mb-3'
+              >
+                {gameTypes.map((type) => (
+                  <option key={type} value={type}>
+                    {type === 'all' ? 'Select All' : type}
+                  </option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+          </Col>
+
+          <Col xs={12} md={5} className='p-4'>
+            {filteredRooms.map((room) => (
+              <Card key={room.id} className={styles.card}>
+                <Card.Header
+                  className={`${styles.room_header} d-flex justify-content-between align-items-center`}
+                >
+                  <span>{room.name}</span>
+                  {room.password ? (
+                    <HiLockClosed title='Password protected' />
+                  ) : (
+                    <HiLockOpen title='No password' />
+                  )}
+                </Card.Header>
+                <Card.Body>
+                  <Card.Text>
+                    <p className={styles.room_text}>
+                      Game : {room.gametype}
+                      <br />
+                      Players : 
+                      {room.current_player}/{room.max_player}
+                      <Button
+                        style={{ float: 'right' }}
+                        variant={
+                          room.current_player === room.max_player
+                            ? 'secondary'
+                            : 'primary'
+                        }
+                        disabled={room.current_player === room.max_player}
+                        onClick={() => handleEnterRoom(room)}
+                      >
+                        Enter
+                      </Button>
+                    </p>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            ))}
+          </Col>
+        </Row>
+
+        {/* Password modal */}
+        <Modal show={showModal} onHide={() => setShowModal(false)} centered>
+          <Modal.Header closeButton>
+            <Modal.Title>Enter Room Password</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form.Group>
+              <Form.Label>Password:</Form.Label>
+              <Form.Control
+                type='password'
+                value={passwordInput}
+                onChange={(e) => setPasswordInput(e.target.value)}
+              />
+            </Form.Group>
+            {errorMessage && (
+              <Alert variant='danger' className='mt-3'>
+                {errorMessage}
+              </Alert>
+            )}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant='secondary' onClick={() => setShowModal(false)}>
+              Cancel
+            </Button>
+            <Button variant='primary' onClick={handlePasswordSubmit}>
+              Enter Room
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </Container>
+    </>
   );
 }
