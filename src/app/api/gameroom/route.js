72619d0b6aacb8d1144db4752a141e0c6092985d
howard522 <8754howard@gmail.com>
2024-10-20 21:34:31 +0800

    feat.gameroom api

--- src/app/api/gameroom/route.js
+++ src/app/api/gameroom/route.js
@@ -1,23 +1,32 @@
-import {database} from "@/utils/firebase.js";
-import {collection, deleteDoc, doc, getDoc, getDocs, query, setDoc, where} from 'firebase/firestore';
-import {v4 as uuidv4} from "uuid";
+import { database } from '@/utils/firebase.js';
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  setDoc,
+  where
+} from 'firebase/firestore';
+import { v4 as uuidv4 } from 'uuid';
 
 export async function POST(request) {
-  const {userId, name, password, type} = await request.json();
+  const { userId, name, password, type } = await request.json();
   const checkRoomnameExists = async (name) => {
     const roomCollection = collection(database, 'room');
-    const roomList = query(roomCollection, where("name", "==", name));
+    const roomList = query(roomCollection, where('name', '==', name));
     const querySnapshot = await getDocs(roomList);
     return !querySnapshot.empty;
   };
-  if(name.length > 15){
-    return Response.json({error: "房間名稱長度不可大於15"}, {status: 400});
+  if (name.length > 15) {
+    return Response.json({ error: '房間名稱長度不可大於15' }, { status: 400 });
   }
   if (await checkRoomnameExists(name)) {
-    return Response.json({error: "此房間名稱已存在"}, {status: 400});
+    return Response.json({ error: '此房間名稱已存在' }, { status: 400 });
   }
   if (password && password.length < 6) {
-    return Response.json({error: '密碼長度不可小於6'}, {status: 400});
+    return Response.json({ error: '密碼長度不可小於6' }, { status: 400 });
   }
   const roomId = uuidv4();
   const roomRef = doc(database, `room/${roomId}`);
@@ -27,47 +36,48 @@ export async function POST(request) {
     type,
     players: {
       [userId]: {
-        place: "1",
-        handCards: [],
-      },
+        place: '1',
+        handCards: []
+      }
     },
+    maxPlayer: 4,
     nowCards: [],
-    state: "waiting",
-    time: new Date().toISOString(),
+    state: 'waiting',
+    time: new Date().toISOString()
   });
-  return Response.json({message: '創建成功', roomId}, {status: 201});
+  return Response.json({ message: '創建成功', roomId }, { status: 201 });
 }
 
-
 export async function DELETE(request) {
-  const {roomId} = await request.json();
+  const { roomId } = await request.json();
   const roomCollection = collection(database, 'room');
   const roomDocRef = doc(roomCollection, roomId);
   const roomDocSnap = await getDoc(roomDocRef);
-  if (!roomDocSnap.exists()) return Response.json({error: `id does not exist`}, {status: 404});
+  if (!roomDocSnap.exists())
+    return Response.json({ error: `id does not exist` }, { status: 404 });
   const roomData = roomDocSnap.data();
   if (roomData.players && Object.keys(roomData.players).length === 0) {
     await deleteDoc(roomDocRef);
-    return Response.json({message: '刪除成功'}, {status: 201});
+    return Response.json({ message: '刪除成功' }, { status: 201 });
   }
-  return Response.json({error: '房間還有玩家'}, {status: 400});
+  return Response.json({ error: '房間還有玩家' }, { status: 400 });
 }
 
 export async function GET(request) {
-  const {searchParams} = new URL(request.url);
+  const { searchParams } = new URL(request.url);
   const id = searchParams.get('id');
   if (id) {
     const docRef = doc(database, `room/${id}`);
     const docSnap = await getDoc(docRef);
     if (docSnap.exists()) {
-      return Response.json(docSnap.data(), {status: 200});
+      return Response.json(docSnap.data(), { status: 200 });
     } else {
-      return Response.json({error: `id does not exist`}, {status: 404});
+      return Response.json({ error: `id does not exist` }, { status: 404 });
     }
   } else {
     const roomCollection = collection(database, 'room');
     const roomList = await getDocs(roomCollection);
-    const rooms = roomList.docs.map(doc => doc.data());
-    return Response.json(rooms, {status: 200});
+    const rooms = roomList.docs.map((doc) => doc.data());
+    return Response.json(rooms, { status: 200 });
   }
 }
