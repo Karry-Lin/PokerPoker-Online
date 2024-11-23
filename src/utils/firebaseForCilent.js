636e6b72020a9e2df0a59be5db1a28f13d940715
Karry-Lin <a06220615@gmail.com>
2024-11-24 00:58:08 +0800

    Update firebaseForCilent.js

--- src/utils/firebaseForCilent.js
+++ src/utils/firebaseForCilent.js
@@ -1,12 +1,17 @@
 import {initializeApp} from "firebase/app";
 import {getFirestore} from "firebase/firestore";
 
+let database = null;
+
 export const getDatabase = async () => {
+  console.log(`getDatabase is null: ${database === null}`);
+  if (database) return database;
   const response = await fetch('/api/firebase');
   const data = await response.json();
   if (!response.ok) {
     throw new Error(data.error);
   }
   const firebase = initializeApp(data);
-  return getFirestore(firebase, '(default)');
+  database = getFirestore(firebase, '(default)');
+  return database;
 };
