6634172006b3c4b644c5ab760e0b78f6e665bbcb
Karry-Lin <a06220615@gmail.com>
2024-11-23 23:27:56 +0800

    fix: firebase config

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,20 +2,26 @@ import {initializeApp} from "firebase/app";
 import {getStorage} from "firebase/storage";
 import {getFirestore} from "firebase/firestore";
 
+const getFirebaseConfig = () => ({
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+});
+
 const getFirebase = () => {
-  const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
-  };
+  const firebaseConfig = getFirebaseConfig();
   return initializeApp(firebaseConfig);
 };
 
-const database = getFirestore(getFirebase(), '(default)');
-const storage = getStorage(getFirebase());
+export const database = getFirestore(getFirebase(), '(default)');
 
-export {database, storage};
+export const getDatabase = () => {
+  const firebase = getFirebase();
+  return getFirestore(firebase, '(default)');
+};
+
+export const storage = getStorage(getFirebase());
