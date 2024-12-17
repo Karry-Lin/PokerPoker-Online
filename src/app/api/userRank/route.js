bccc6ca8287fba545f29d314281dca414bf9227b
Karry-Lin <a06220615@gmail.com>
2024-12-17 17:13:38 +0800

    Update route.js

--- src/app/api/userRank/route.js
+++ src/app/api/userRank/route.js
@@ -3,11 +3,23 @@ import {collection, getDocs, orderBy, query} from "firebase/firestore";
 
 export async function GET(request) {
     try {
-        const usersCollectionRef = collection(database, 'user');
-        const querySnapshot = await getDocs(query(usersCollectionRef, orderBy('money', 'desc')));
-        const users = querySnapshot.docs.map((doc) => doc.data());
-        users.sort((a, b) => b.money - a.money);
-        return Response.json(users, { status: 200 });
+        const {searchParams} = new URL(request.url);
+        const id = searchParams.get('id');
+        if(id === 'user')
+        {
+            const usersCollectionRef = collection(database, 'user');
+            const querySnapshot = await getDocs(query(usersCollectionRef, orderBy('money', 'desc')));
+            const users = querySnapshot.docs.map((doc) => doc.data());
+            users.sort((a, b) => b.money - a.money);
+            return Response.json(users, { status: 200 });
+        }
+        if(id === "room")
+        {
+            const roomCollection = collection(database, 'room');
+            const roomList = await getDocs(query(roomCollection, orderBy('time', 'desc')));
+            const rooms = roomList.docs.map((doc) => doc.data());
+            return Response.json(rooms, {status: 200});
+        }
     } catch (error) {
         console.error("Error fetching user data:", error);
         return Response.json({ error: "Failed to fetch user data" }, { status: 500 });
